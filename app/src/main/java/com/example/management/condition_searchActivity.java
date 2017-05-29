3347d399d8a4ea43f4f332bccfc97091f7163ea3
PSA <zado01.naver.com>
2017-05-29 20:57:30 +0900

    질병검색 레이아웃

---
+++ app/src/main/java/com/example/management/condition_searchActivity.java
@@ -0,0 +1,13 @@
+package com.example.management;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class condition_searchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condition_search);
+    }
+}
---
+++ app/src/main/java/com/example/management/disease_condition.java
@@ -0,0 +1,13 @@
+package com.example.management;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class disease_condition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease_condition);
+    }
+}
