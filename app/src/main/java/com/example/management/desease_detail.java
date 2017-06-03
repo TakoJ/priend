df821b338dd662bad6f24ed1c3317aec3b75c64d
Jiyeon <hgy3047@gmail.com>
2017-06-04 02:31:29 +0900

    disease intent연결, adding activity in manifest

---
+++ app/src/main/java/com/example/management/desease_detail.java
@@ -0,0 +1,13 @@
+package com.example.management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class desease_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desease_detail);
+    }
+}
--- app/src/main/java/com/example/management/disease_condition.java
+++ app/src/main/java/com/example/management/disease_condition.java
@@ -1,13 +1,44 @@
 package com.example.management;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class disease_condition extends AppCompatActivity {
+
+    private Button condition;
+    private Button disease_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disease_condition);
+
+        condition = (Button) findViewById(R.id.condition);
+        disease_name = (Button) findViewById(R.id.disease_name);
+
+
+        condition.setOnClickListener(new View.OnClickListener(){
+            //질병명 검색 눌렀을 때
+            @Override
+            public void onClick(View v) {
+                if (v == condition) {
+                    Intent intent = new Intent(getApplicationContext(), disease_searchActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        disease_name.setOnClickListener(new View.OnClickListener(){
+            //증상으로 검색 눌렀을 때
+            @Override
+            public void onClick(View v) {
+                if (v == disease_name) {
+                    Intent intent = new Intent(getApplicationContext(), condition_searchActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
